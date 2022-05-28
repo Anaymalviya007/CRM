933e5615f43293cb7f3099cf5ea6b7bd9dd5a346
Anaymalviya007 <anaymalviya900@gmail.com>
2022-05-28 11:33:46 +0530

    CRM

---
+++ src/components/ContentContainer/ContentContainer.jsx
@@ -0,0 +1,24 @@
+import TopNavigation from '../TopNavigation';
+
+
+const ContentContainer = () => {
+  return (
+    <div className='content-container'>
+      <TopNavigation />
+      <div className='content-list'>
+        
+       
+        
+      </div>
+      
+    </div>
+  );
+};
+
+
+
+
+
+
+
+export default ContentContainer;
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,16 +1,17 @@
-import "./Dashboard.css"
 import Sidebar from '../../components/sidebar/Sidebar'
-import Navbar from '../../components/navbar/Navbar'
+import ContentContainer from "../../components/ContentContainer/ContentContainer"
+import ChannelBar from '../../components/ChannelBar/ChannelBar'
 
 const Dashboard = () => {
   return (
-    <div className='dashboard'>  
-      
-        <Navbar />
+    <div className='flex'>    
         <Sidebar />
+        {/* <ChannelBar /> */}
+        <ContentContainer />
     </div>
   )
 }
 
 
 export default Dashboard
+ 
